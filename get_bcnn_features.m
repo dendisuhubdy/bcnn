c72a6d7dd437648e243f3f7a3b1980ad28cfb6aa
Tsung-Yu Lin <tsungyulin@cs.umass.edu>
2017-12-07 14:02:56 -0500

    add cropping for get_bcnn_feature

--- get_bcnn_features.m
+++ get_bcnn_features.m
@@ -118,26 +118,25 @@ end
 
 
 
-function im_resized = preprocess_image(im, keepAspect, imageSize, averageColour, scale)
+function im_resized = preprocess_image(im, keepAspect, imageSize, averageColour, scale, border)
+
+w = size(im,2) ;
+h = size(im,1) ;
+factor = [(imageSize(1)+border(1))/h, (imageSize(2)+border(2))/w]*scale;
 
 if keepAspect
-    w = size(im,2) ;
-    h = size(im,1) ;
-    
-    factor = max(factor)*scale ;
-    
-    im_resized = imresize(single(im), ...
-        'scale', factor, ...
-        'method', 'bilinear') ;
-    
-    w = size(im_resized,2) ;
-    h = size(im_resized,1) ;
-    
-    im_resized = imcrop(im_resized, [fix((w-imageSize(1)*scale)/2)+1, fix((h-imageSize(2)*scale)/2)+1,...
-        round(imageSize(1)*scale)-1, round(imageSize(2)*scale)-1]);
-else
-    im_resized = imresize(single(im), round(imageSize([2 1])*scale), 'bilinear');
+    factor = max(factor);
 end
+
+im_resized = imresize(single(im), ...
+    'scale', factor, ...
+    'method', 'bilinear') ;
+
+w = size(im_resized,2) ;
+h = size(im_resized,1) ;
+
+im_resized = imcrop(im_resized, [fix((w-imageSize(1)*scale)/2)+1, ...
+            fix((h-imageSize(2)*scale)/2)+1, ...
+            round(imageSize(1)*scale)-1, round(imageSize(2)*scale)-1]);
+
 im_resized = bsxfun(@minus, im_resized, averageColour) ;
-
-
